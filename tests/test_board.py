7ef65e4a92c8ed8dee72795edabf6eef28db4d06
Sten Remmelg <sten.remmelg@gmail.com>
2021-08-21 17:55:10 +0200

    add tests for board state checking

--- tests/test_board.py
+++ tests/test_board.py
@@ -10,3 +10,25 @@ class TestSudokuBoard:
 
     def test_correct_shape_raises_no_error(self):
         SudokuBoard(np.ones((9, 9)))
+
+    def test_board_state_is_valid(self):
+        board = SudokuBoard(
+            np.array(
+                [
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+                    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+                    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+                    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+                    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+                    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+                    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+                ]
+            )
+        )
+        assert board.state_is_valid == True
+
+    def test_board_state_is_invalid(self):
+        board = SudokuBoard(np.ones((9, 9)))
+        assert board.state_is_valid == False
