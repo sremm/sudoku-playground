95e2897464e63d48a43aa285bf31fe4a145463b1
Sten Remmelg <sten.remmelg@qualisys.se>
2022-11-20 20:07:09 +0100

    add easy sudoku to test cases

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -5,7 +5,7 @@ from sudoku.solver import SudokuSolver
 
 
 class TestSudokuSolver:
-    def test_simple_board_1_missing_but_state_invalid(self):
+    def test_state_invalid(self):
         board = SudokuBoard(
             np.array(
                 [
@@ -40,6 +40,28 @@ class TestSudokuSolver:
                     [3, 4, 5, 6, 7, 8, 9, 1, 2],
                     [6, 7, 8, 9, 1, 2, 3, 4, 5],
                     [9, 1, 2, 3, 4, 5, 6, 7, 0],
+                ]
+            )
+        )
+        assert board.is_solved == False
+
+        result_board, result_message = SudokuSolver.solve(board)
+        assert result_board.state_is_valid == True
+        assert result_board.is_solved == True
+
+    def test_easy_sudoku(self):
+        board = SudokuBoard(
+            np.array(
+                [
+                    [0, 0, 6, 8, 7, 1, 0, 0, 3],
+                    [0, 7, 3, 0, 5, 6, 1, 9, 9],
+                    [0, 0, 0, 3, 4, 9, 0, 2, 7],
+                    [3, 4, 2, 0, 0, 0, 0, 8, 0],
+                    [0, 6, 0, 0, 2, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 3, 0, 5, 2],
+                    [0, 1, 0, 7, 0, 4, 8, 0, 0],
+                    [7, 0, 0, 5, 9, 8, 2, 6, 1],
+                    [0, 0, 5, 0, 0, 0, 9, 0, 0],
                 ]
             )
         )
