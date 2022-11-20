787d2aa473e40cc601d46744a65fc2b5b518d147
Sten Remmelg <sten.remmelg@qualisys.se>
2022-11-20 19:34:11 +0100

    add new happy path test and adjust existing test to be a non happy path test

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -6,5 +6,9 @@ from sudoku.board import SudokuBoard
 class SudokuSolver:
     @staticmethod
     def solve(board: SudokuBoard) -> Tuple[SudokuBoard, str]:
+        """Recursion brute force"""
+        message = ""
+        if board.state_is_valid == False:
+            message = "SOLVER ERROR - Board state is invalid, cannot solve an invalid board"
 
-        return (board, "message from solver")
+        return (board, message)
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -5,7 +5,7 @@ from sudoku.solver import SudokuSolver
 
 
 class TestSudokuSolver:
-    def test_simple_board_1_missing(self):
+    def test_simple_board_1_missing_but_state_invalid(self):
         board = SudokuBoard(
             np.array(
                 [
@@ -21,6 +21,30 @@ class TestSudokuSolver:
                 ]
             )
         )
+        assert board.is_solved == False
+
+        result_board, result_message = SudokuSolver.solve(board)
+        assert "SOLVER ERROR" in result_message
+        assert result_board.is_solved == False
+
+    def test_simple_board_1_missing(self):
+        board = SudokuBoard(
+            np.array(
+                [
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+                    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+                    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+                    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+                    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+                    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+                    [9, 1, 2, 3, 4, 5, 6, 7, 0],
+                ]
+            )
+        )
+        assert board.is_solved == False
+
         result_board, result_message = SudokuSolver.solve(board)
         assert result_board.state_is_valid == True
         assert result_board.is_solved == True
